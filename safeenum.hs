653b92f4d2dd11a7e66b2885cdd2090f82fd27fa
Alex Chistyakov <alexclear@gmail.com>
2017-02-21 09:00:31 +0300

    Solve more tasks

---
+++ safeenum.hs
@@ -0,0 +1,5 @@
+class (Eq a, Enum a, Bounded a) => SafeEnum a where
+  ssucc :: a -> a
+  spred :: a -> a
+  ssucc = \a -> if maxBound == a then minBound else succ a
+  spred = \a -> if minBound == a then maxBound else pred a
